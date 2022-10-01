90f206acad8dec094cacd4b80d56e628c0651e67
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-10-01 02:47:45 -0300

    feat :sparkles: finished user provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Router from "./routes/router"
 import style from "./style"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   return (
     <div className={`${style.flexCenter}`}>
+      <Toaster position="top-right" />
       <Router/>
     </div>
   )
--- src/providers/user/index.tsx
+++ src/providers/user/index.tsx
@@ -1,19 +1,51 @@
 import { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
-interface UserContextInterface {
-    isLoggedIn: any;
-    setIsLoggedIn: any
-}
-interface UserProviderProps {
-    children: React.ReactNode
-}
+import api from "../../api"
+import { useNavigate } from "react-router-dom"
+import toast from "react-hot-toast"
+import { IUserLogin, IUserRegister, UserContextInterface, UserProviderProps } from "../../interfaces/User";
+
 
 export const UserContext = createContext<UserContextInterface>({} as UserContextInterface);
 
 export const UserProvider = ({ children }: UserProviderProps) => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        const token = JSON.parse(localStorage.getItem("@BearerToken") || '{}')
+        if (token.token) {
+            return setIsLoggedIn(true)
+        }
+    }, [isLoggedIn])
+
+    const login = async (data: IUserLogin) => {
+        await api.post('/user/login', data)
+        .then((res) => {
+            localStorage.setItem("@BearerToken", JSON.stringify(res.data))
+            setIsLoggedIn(true)
+            return navigate("/")
+        })
+        .catch((_) => toast.error("E-mail ou senha invalido!"))
+    }
+
+    const register = async (data: IUserRegister) => {
+        const loginData = {
+            email: data.email,
+            password: data.password
+        }
+
+        await api.post("/user", data)
+        .then((res) => {
+            toast.success("Usuário criado com sucesso!")
+            login(loginData)
+        })
+        .catch((_) => toast.error("Não foi possivel realizar o cadastro."))
+    }
+
+
     return (
-        <UserContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+        <UserContext.Provider value={{isLoggedIn, setIsLoggedIn, login, register}}>
             {children}
         </UserContext.Provider>
     )
